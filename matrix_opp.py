03828336a344e60f3cbae1cfa0660a58e868e69a
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-10-18 15:44:29 +0530

    Add files via upload

---
+++ matrix_opp.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+mat1 = np.array([[10,20,30],[40,50,60],[70,80,90]])
+print("size",mat1.size)
+print("Array type", type(mat1))
+print("shape", mat1.shape)
+print("dimensions", mat1.ndim)
+print("data type ", mat1.dtype)
